e2ec8cebf52eb003fbc420cdfc19876e36fe111d
tauqeerkhan24 <tauqeerkhan24@gmail.com>
2016-05-31 05:09:39 +0000

    Add files via upload

---
+++ main/java/mobiletesting/transportworld/Welcomepage.java
@@ -0,0 +1,24 @@
+package mobiletesting.transportworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcomepage extends AppCompatActivity {
+    Button button_sbm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomepage);
+        button_sbm =(Button)findViewById(R.id.button);
+        button_sbm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("mobiletesting.transportworld.Homepage");
+                startActivity(intent);
+            }
+        });
+
+    }}
